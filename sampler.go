30bc7505320b2f1a422f9da46f0a7dc8a717d1c9
orion <oletizi@mac.com>
2020-09-22 17:29:51 -0700

    - woohoo! reading a midi file!

--- sampler.go
+++ sampler.go
@@ -9,15 +9,17 @@ import (
 type printer struct{}
 
 func (pr printer) noteOn(p *reader.Position, channel, key, vel uint8) {
-
+	log.Printf("note ON: pos: %d, chan: %d, key: %v, vel: %d ", p.AbsoluteTicks, channel, key, vel)
 }
 
-func LoadFile(filename string) {
-	//var p printer
-	//
-	//rd := reader.New(reader.NoLogger(),
-	//	reader.NoteOn(p.noteOn),
-	//	)
-	//err = rd.ReadSMFFile(rd, f)
+func LoadFile(filename string) error {
 	log.Printf("LoadFile: %v", filename)
+
+	var p printer
+
+	rd := reader.New(reader.NoLogger(),
+		reader.NoteOn(p.noteOn),
+	)
+
+	return reader.ReadSMFFile(rd, filename)
 }
